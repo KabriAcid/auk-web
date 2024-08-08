3ae019d7507c04f3b0e8fe3cd06fef3f1ca8ea9a
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-08 12:31:39 +0100

    "Added favicon link to welcome.blade.php"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Al-Qalam University Katsina</title>
+    <!-- favicon -->
+     <link rel="shortcut icon" href="{{ asset('images/logo.jpg') }}" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
