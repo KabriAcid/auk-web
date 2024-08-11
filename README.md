fe7dd1d6c32614af1d6760bf2d3c6c1bfa1eea32
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-11 17:33:44 +0100

    update READme.md

--- README.md
+++ README.md
@@ -17,14 +17,14 @@ The goal of this project is to enhance the current AUK website by improving its 
 To maintain consistency and clarity in our version control, we follow these branch naming conventions:
 
 ### General Guidelines
-- Use **lowercase letters** and **hyphens** to separate words (e.g., `feature/add-login-page`).
+- Use **lowercase letters** and **hyphens** to separate words (e.g., `feature/username`).
 - Start branch names with a **type prefix** to categorize the branch.
 
 ### Prefixes
 - **feature/**: New features or enhancements.
   - Example: `feature/user-authentication`
-- **bugfix/**: Fixes for non-critical issues.
-  - Example: `bugfix/fix-footer-alignment`
+- **ui/**: Fixes for non-critical issues.
+  - Example: `ui/username`
 - **hotfix/**: Urgent or critical fixes that need to be addressed immediately.
   - Example: `hotfix/security-patch`
 - **chore/**: Maintenance tasks or refactoring.
@@ -32,8 +32,8 @@ To maintain consistency and clarity in our version control, we follow these bran
 
 
 ### Examples
-- `feature/add-login-functionality`
-- `bugfix/5678-fix-crash-on-login`
+- `feature/KabriAcid`
+- `ui/5678-fix-crash-on-login`
 - `hotfix/urgent-security-patch`
 - `release/1.2.0`
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Al-Qalam University Katsina</title>
     <!-- favicon -->
-     <link rel="shortcut icon" href="{{ asset('images/logo-removebg.png') }}" type="image/x-icon">
+     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,15 +15,15 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
-<body class="flex justify-center items-center h-screen bg-gray-100" style="font-family: figtree;">
+<body class="flex justify-center items-center h-screen bg-gray-100" style="font-family: figtree;">  
     <div class="bg-white shadow-lg rounded-xl w-1/2 text-center p-8">
         <div class="card-header mb-4">
-            <img src="{{ asset('images/logo-removebg.png') }}" alt="Logo" class="mx-auto mb-4 w-24 h-24">
+            <img src="{{ asset('images/favicon.png') }}" alt="Logo" class="mx-auto mb-4 w-24 h-24">
             <h1 class="text-2xl font-bold" style="color: #94152b;">AL-QALAM UNIVERSITY KATSINA</h1>
             <p class="text-sm text-gray-500">ALONG DUTSINMA ROAD, KATSINA, KATSINA.</p>
         </div>
         <div class="card-body mb-4">
-            <p class="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur eius accusamus alias facere aspernatur iusto veritatis non dignissimos, labore, quam sed consequuntur?.</p>
+            <p class="text-lg" style="color: green";>Laravel has been installed successfully.</p>
         </div>
         <div class="card-footer text-gray-600 text-sm">
             <p>&copy; Copyright <?= date('Y');?> @ AUK</p>
