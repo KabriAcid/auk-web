413016987f3512280b5e0c215a7e616f0ad80573
K-Musty <kmustapha9564@gmail.com>
2024-08-13 15:25:27 +0100

    Made some few changes to the migrations file

--- database/migrations/2024_08_11_230934_create_programs_table.php
+++ database/migrations/2024_08_11_230934_create_programs_table.php
@@ -1,27 +1,21 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProgramsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('programs', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // Primary Key with auto-increment
+            $table->string('name', 50);
+            $table->timestamps(); // Adds created_at and updated_at columns
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('programs');
     }
-};
+}
