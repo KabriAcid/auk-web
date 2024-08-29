5d7fb47dc0adda84554f833bd1d42a65e4e1ffd3
Musajidda <muhammadmjidder8@gmail.com>
2024-08-29 12:50:49 +0100

    i Implemented a staff list retrieval and staff creation in StaffController.I Set up a model interaction with the database and add validation for staff creation. Currently the staff list is displayed dynamically from the database, i added an 'add.blade.php and a list.blade.php into a new staff folder under 'resources/views'

---
+++ resources/views/staff/list.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff List</title>
+</head>
+<body>
+    <h1>Staff List</h1>
+
+    @if (session('success'))
+        <p>{{ session('success') }}</p>
+    @endif
+
+    <ul>
+        @foreach ($staff as $member)
+            <li>{{ $member->name }} - {{ $member->email }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
