bae13d2b07d3f612bae70180e9180367e1ec69a1
Abdullahi Kabri <kabriacid01@gmail.com>
2024-09-03 00:38:22 +0100

    "Deleted migration file '2024_08_29_104125_create_staff_table.php'"

--- database/migrations/2024_09_02_233646_create_colleges_table.php
+++ database/migrations/2024_09_02_233646_create_colleges_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('staff', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();   
-            $table->string('phone_number')->nullable();
+        Schema::create('colleges', function (Blueprint $table) {
+            $table->id('college_id');
+            $table->string('college_name');
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('staff');
+        Schema::dropIfExists('colleges');
     }
 };
