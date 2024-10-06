bca283a8bf5ee1f7fcbffb5ba8975e121aed2a85
Musajidda <muhammadmjidder8@gmail.com>
2024-10-06 17:42:19 +0100

    some changes in admin.css, style.css,and  carousel.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+document.addEventListener("DOMContentLoaded", function() {
+    const carouselSection = document.getElementById("carouselSection");
+    carouselSection.style.marginTop = "0px";
+});
--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,27 +1,33 @@
 <section id="carouselSection">
-            <h2>Our Campus</h2>
-            <div id="campusCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="{{ asset('images\IMG_0030.JPG') }}" class="d-block w-100" alt="carousel Image 1">
+    <link rel="stylesheet" href="public/css/style.css">
+   
+    <div id="campusCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Dot navigation indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#campusCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#campusCarousel" data-slide-to="1"></li>
+            <li data-target="#campusCarousel" data-slide-to="2"></li>
+        </ol>
 
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('images\IMG_0524.JPG') }}" class="d-block w-100" alt="carousel Image 2">
-
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('images\IMG_0501.JPG') }}" class="d-block w-100" alt="carousel Image 3">
-
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#campusCarousel" role="button" data-slide="prev">
-                    <i class='bx bx-chevron-left' aria-hidden="true"></i>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#campusCarousel" role="button" data-slide="next">
-                    <i class='bx bx-chevron-right' aria-hidden="true"></i>
-                    <span class="sr-only">Next</span>
-                </a>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('images/IMG_0030.JPG') }}" class="d-block" alt="carousel Image 1">
             </div>
-        </section>
+            <div class="carousel-item">
+                <img src="{{ asset('images/IMG_0524.JPG') }}" class="d-block" alt="carousel Image 2">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('images/IMG_0501.JPG') }}" class="d-block" alt="carousel Image 3">
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#campusCarousel" role="button" data-slide="prev">
+            <i class='bx bx-chevron-left' aria-hidden="true"></i>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#campusCarousel" role="button" data-slide="next">
+            <i class='bx bx-chevron-right' aria-hidden="true"></i>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
